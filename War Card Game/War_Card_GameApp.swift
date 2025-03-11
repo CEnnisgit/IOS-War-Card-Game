cad45751e74d6a1439c9441f1bd64d7c67927755
Chris Ennis <cennis512@gmail.com>
2025-03-11 17:43:25 -0400

    Initial commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Chris on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
